250cf082fc1a0da6480ceeceb139ee02dbcccff3
coflyd <146206916+coflyd@users.noreply.github.com>
2026-02-09 18:50:56 -0500

    Update Map.h
    Add the MapLoader to Map.h
    Then modify Map so that it uses a file of type MapLoader

--- Maps (Part1)/Map.h
+++ Maps (Part1)/Map.h
@@ -1,6 +1,7 @@
 #ifndef MAP_H_
 #define MAP_H_
 
+#include <iostream>
 #include <map>
 #include <string>
 #include <vector>
@@ -21,17 +22,40 @@ struct Country {
 	int y;
 };
 
+class MapLoader {
+private : 
+	std::vector<std::string> files; 
+	std::vector<std::unique_ptr<Continent>> continents; 
+	std::vector<std::uniqu_ptr<Continent>> countries; 
+	std::vector<int> borders; 
+
+public : 
+	MapLoader(const std::string &mapFileName);
+	const std::vector<int> &getBorders();
+	const std::vector<std::unique_ptr<Continent>> &getContinents(); 
+	const std::vector<std::unique_ptr<Country>> &getCountries();
+
+	MapLoader(const MapLoader &other);
+	MapLoader &operator=(const MapLoader &other);
+	friend std::ostream &operator<<(std::ostream &os, const MapLoader &map);
+	void display();
+};
+
 class Map {
 private:
-	std::vector<std::string> files;
-	std::vector<std::unique_ptr<Continent>> continents;
-	std::vector<std::unique_ptr<Country>> countries;
-	std::vector<int> borders;
+	//std::vector<std::string> files;
+	//std::vector<std::unique_ptr<Continent>> continents;
+	//std::vector<std::unique_ptr<Country>> countries;
+	std::unique_ptr<MapLoader> mapObjects;
 	std::map<int, std::vector<int>> countriesAdjGraph;
 
 public:
-	Map(const std::string &mapFileName);
-	void display(); 
+	Map(std::unique_ptr<MapLoader> mapObjects);
+	Map(const Map &other);
+	Map &operator=(const Map &other);
+	friend std::ostream &operator<<(std::ostream &os, const Map &map);
+	void display();
+	bool isGraphConnected();
 };
 
 #endif /* MAP_H_ */
