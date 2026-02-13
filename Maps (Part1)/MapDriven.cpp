ec77aeefa852d0ce92d82c2f0f57bc26ca374f75
coflyd <146206916+coflyd@users.noreply.github.com>
2026-02-13 13:18:01 -0500

    Update MapDriven.cpp

--- Maps (Part1)/MapDriven.cpp
+++ Maps (Part1)/MapDriven.cpp
@@ -1,19 +1,25 @@
 #include "Map.h"
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-	//auto mapLoader = std::make_unique<MapLoader>("data/europe_map/europe.map");
-	auto mapLoader = std::make_unique<MapLoader>("data/big_europe_map/bigeurope.map");
+
+	std::vector<std::unique_ptr<Player>> players;
+	for(int i = 1;i<=4;i++){
+		players.push_back(std::make_unique<Player>(i, "player_"+std::to_string(i)));
+	}
+	for (const auto &p:players){
+		cout << "Player " << p->getId()<< ", pseudo : "<<p->getPseudo()<<std::endl;
+	}
+	auto mapLoader = std::make_unique<MapLoader>("data/europe_map/europe.map");
 	Map europe(std::move(mapLoader));
+	europe.initialCountryDistribution(players);
 	europe.display();
 	cout << europe<<endl;
-	if (europe.isGraphConnected()){
-		cout << "Map is connected"<<endl;
-	}else{
-		cerr << "Map is not connected"<<endl;
-	}
-	cout << "\nVerification by continent"<<endl;
-	europe.isContinentsSubGraphsConnected();
-	return 0;
+
+	europe.validate();
+	
+	return (0);
 }
