7ea75701e78e054deccc2e321b8d40c709aa1730
Tony Yacoub <tonywessly@gmail.com>
2026-02-15 05:45:32 -0500

    Update CardsDriver.cpp

--- A1_Part4/CardsDriver.cpp
+++ A1_Part4/CardsDriver.cpp
@@ -1,41 +1,58 @@
-
 #include <iostream>
 #include "Cards.h"
+#include "Player.h"
+#include "Orders.h"
 
 using namespace std;
+
 int main() {
+
     Deck deck;
-    Hand hand;
-    
-    deck.returnCard(new Card(CardType::bomb));
-    deck.returnCard(new Card(CardType::reinforcement));
-    deck.returnCard(new Card(CardType::blockade));
-    deck.returnCard(new Card(CardType::airlift));
-    deck.returnCard(new Card(CardType::diplomacy));
-    deck.returnCard(new Card(CardType::bomb));
-    deck.returnCard(new Card(CardType::reinforcement));
-    deck.returnCard(new Card(CardType::blockade));
-    deck.returnCard(new Card(CardType::airlift));
-    deck.returnCard(new Card(CardType::diplomacy));
-    
-    cout << "Deck size after creation: " << deck.size() << endl;
+    Player player("Joe");
 
+    // Fill deck with 10 cards, 2 of each type
+    deck.returnCard(new Card(CardType::bomb));
+    deck.returnCard(new Card(CardType::reinforcement));
+    deck.returnCard(new Card(CardType::blockade));
+    deck.returnCard(new Card(CardType::airlift));
+    deck.returnCard(new Card(CardType::diplomacy));
+    deck.returnCard(new Card(CardType::bomb));
+    deck.returnCard(new Card(CardType::reinforcement));
+    deck.returnCard(new Card(CardType::blockade));
+    deck.returnCard(new Card(CardType::airlift));
+    deck.returnCard(new Card(CardType::diplomacy));
+    
+    cout << "Initial deck size: " << deck.size() << "\n";
+    cout << "Initial hand size: " << player.getHand()->size() << "\n";
+    cout << "Initial orders size: " << player.getOrders()->size() << "\n\n";
+
+    cout << "Drawing 5 cards into player's hand\n";
     for (int i = 0; i < 5; i++) {
-            Card* c = deck.draw();
-                hand.addCard(c);
-        }
-    cout << "Deck size after drawing: " << deck.size() << endl;
-    cout << "Hand size after drawing: " << hand.size() << endl;
+        Card* c = deck.draw();
+        
+        //  "if (c)" makes sure that the c getting drawn is not a nullptr
+        if (c) player.getHand()->addCard(c);
+    }
 
-    while (hand.size() > 0) {
-            Card* c = hand.getCards()->back();
-            hand.removeCard(c);
-            c->play(nullptr, &deck);
-        }
+    cout << "After draw deck size: " << deck.size() << "\n";
+    cout << "After draw hand size: " << player.getHand()->size() << "\n";
+    cout << "After draw orders size: " << player.getOrders()->size() << "\n\n";
 
-        cout << "Hand size after playing: " << hand.size() << endl;
-        cout << "Deck size after returning: " << deck.size() << endl;
-
+    cout << "Playing all cards\n";
     
+    while (player.getHand()->size() > 0) {
+        
+        //  Returning the top card from the player's hand, which is the last card in the vector
+        Card* c = player.getHand()->getCards()->back();
+        c->play(&player, &deck);
+    }
+
+    cout << "After play deck size: " << deck.size() << "\n";
+    cout << "After play hand size: " << player.getHand()->size() << "\n";
+    cout << "After play orders size: " << player.getOrders()->size() << "\n\n";
+
+    cout << "OrdersList content:\n";
+    cout << *player.getOrders() << "\n";
+
     return 0;
 }
